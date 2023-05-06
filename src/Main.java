babcc9097062e1b22aed5f2f497d8af9101bdd93
ovirto <ovirto@my.dom.edu>
2023-05-06 05:06:31 -0500

    Created Dataload and RandomNumber methods, updated WordPicker method.

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,31 @@ Date: 02-01-2023
 Purpose: This program draws inspiration from the popular word game "Wordle"
 Objective is to guess the secret (5-letter) word within 6 tries.
 
-FIXME: Determine if exit codes are needed.
+EXIT CODES:
+1:  IO Exception
  */
 import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
         final int MAX_GUESSES = 6;
         final int MAX_LENGTH = 5;
         String secretWord = "";
-        secretWord = WordPicker();
+        try {
+            secretWord = DataLoad();
+        } catch (IOException e) {
+            System.err.printf("**  ERROR: IO exception: %s", e);
+            System.exit(1);
+        }
 
         Scanner sc = new Scanner(System.in);
 
         String guess;
         System.out.printf("Welcome to Wurdle. You get %d guesses. Go!\n", MAX_GUESSES);
 
-        //Board at the start of the game
+        //Board at the start of the game TODO: make method for this
         String[] board = {"*****", "*****", "*****", "*****", "*****","*****"};
 
         int i = 0;
@@ -40,15 +46,15 @@ public class Main {
                 i++;
 
                 WinCheck(guess, secretWord);
-                GuessCheck(i, MAX_GUESSES, secretWord);
+                GuessCheck(i, secretWord);
             }
         }
         sc.close();
     }
 
 
-    //Loads database/picks random word to set as secretWord
-    private static String WordPicker() throws IOException {
+    //Loads db/random word set as secretWord
+    private static String DataLoad() throws IOException {
         File f = new File("5 Letter Words.txt");
         String[] words = new String[496];
         int i = 0;
@@ -59,24 +65,29 @@ public class Main {
         }
         sc.close();
 
-        String s;
-        int min = 0;
-        int max =495;
-        int range = max - min + 1;
-        int rand = (int)(Math.random() * range) + min;
-        s = words[rand];
+        return WordPicker(words);
+    }
+
+
+    //Uses word picker method to pick random word in database
+    private static String WordPicker(String[] words){
+        String s = words[RandomNumber()];
         s = s.toUpperCase();
+        //TODO: Remove if no longer needed: System.out.println(s);
         return s;
     }
 
 
-    //Generates Random Number within words array range//FIXME: get this to replace lines 62-69
+    //Generates Random Number within words array range
     private static int RandomNumber(){
-        return 0;
+        int min = 0;
+        int max = 495;
+        int range = max-min + 1;
+        return (int)(Math.random() * range) + min;
     }
 
 
-    //Takes a String and prints it to the console in color.
+    //Prints String to console in color.
     //"Correct" letters will be green if they are in the word, in the right space.
     //Letters will be yellow if they are contained within the word but in the wrong space.
     public static String ColorPrinter(String guess, String secretWord){
@@ -105,7 +116,7 @@ public class Main {
     //Builds string with colors changed accordingly
     private static String TextColor(char guessChar, String secretWord, int count, String s){
 
-        if(Character.compare(guessChar, secretWord.charAt(count)) == 0){
+        if(guessChar == secretWord.charAt(count)){
             return GreenText(guessChar);
         } else if(secretWord.contains(s)){
             return YellowText(guessChar);
@@ -114,13 +125,13 @@ public class Main {
     }
 
 
-    //Turns text green
+    //Green console text
     private static String GreenText(char c){
         return "\033[0;32m" + c + "\033[0m";
     }
 
 
-    //Turns text yellow
+    //Yellow console text
     private static String YellowText(char c) {
         return "\033[0;33m" + c + "\033[0m";
     }
@@ -136,8 +147,8 @@ public class Main {
 
 
     //Ends game if all guesses have been used.
-    private static void GuessCheck(int i, int max, String s){
-        if (i == max){
+    private static void GuessCheck(int i, String s){
+        if (i == 6){
             System.out.println("Out of guesses. Better luck next time!");
             System.out.printf("Correct answer was: %s\n", s);
             System.exit(0);
